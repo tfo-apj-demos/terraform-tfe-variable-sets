52e50e3ade11c66898f95a3f32a7e24965c858ce
Grant Orchard <go@hashicorp.com>
2024-01-11 17:40:41 +1100

    initial commit

---
+++ 10 - vcenter_creds.tf
@@ -0,0 +1,31 @@
+module "varset" {
+  source  = "app.terraform.io/tfo-apj-demos/varsets/tfe"
+  version = "0.0.1"
+  
+  tfc_organization = "tfo-apj-demos"
+  varset_name = "vcenter_credentials"
+  description = "Credentials for vCenter, rotated daily."
+
+  varset_variables = [
+    {
+      name = "VSPHERE_USER"
+      value = data.vault_generic_secret.this.data.username
+      category = "env"
+    },
+    {
+      name = "VSPHERE_PASSWORD"
+      value = data.vault_generic_secret.this.data.password
+      category = "env"
+      sensitive = true
+    },
+    {
+      name = "VSPHERE_SERVER"
+      value = var.vcenter_address
+      category = "env"
+    }
+  ]
+}
+
+data "vault_generic_secret" "this" {
+  path = "ldap/creds/vsphere_access"
+}
---
+++ 90 - variables.tf
@@ -0,0 +1,4 @@
+variable "vcenter_address" {
+  type = string
+  description = "vCenter server address."
+}
