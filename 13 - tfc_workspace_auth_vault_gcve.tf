074dc54c281e0f89b2aae900f5fea6b3b55f1f51
Grant Orchard <go@hashicorp.com>
2024-02-29 16:40:34 +1100

    update vault identity variables

--- 13 - tfc_workspace_auth_vault_gcve.tf
+++ 13 - tfc_workspace_auth_vault_gcve.tf
@@ -5,16 +5,17 @@ locals {
     "TFC_VAULT_PROVIDER_AUTH",
     "TFC_VAULT_RUN_ROLE",
     "TFC_VAULT_WORKLOAD_IDENTITY_AUDIENCE",
-    "TFC_VAULT_ENCODED_CACERT"
+    "TFC_VAULT_ENCODED_CACERT",
   ]
 }
 
 module "workspace_auth_vault_gcve" {
   source  = "app.terraform.io/tfo-apj-demos/varsets/tfe"
-  version = "0.0.1"
+  version = "~> 0.0"
 
   tfc_organization = "tfo-apj-demos"
-  varset_name = "gcve_workspace_identity_tfc"
+  varset_name = "__gcve_workspace_identity_tfc"
+  workspace_tags = ["vault-gcve"]
 
   varset_variables = [ for variable_name in local.gcve_workspace_identity_tfc_vars: {
       name = "${variable_name}"
