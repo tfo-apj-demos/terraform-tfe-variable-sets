4c6d42132870b81db90da60411b306f245bf61f7
Grant Orchard <go@hashicorp.com>
2024-05-22 10:35:38 +1000

    add varset for Boundary service account

--- 14 - boundary_service_account.tf
+++ 14 - boundary_service_account.tf
@@ -16,14 +16,14 @@ module "boundary_service_account" {
 
   varset_variables = [ for variable_name in local.boundary_service_account_vars: {
       name = "${variable_name}"
-      value = data.hcp_vault_secrets_secret.this["${variable_name}"].secret_value
+      value = data.hcp_vault_secrets_secret.boundary_service_account["${variable_name}"].secret_value
       category = "terraform"
     }
   ]
   apply_to_workspace_tags = ["boundary_management"]
 }
 
-data "hcp_vault_secrets_secret" "this" {
+data "hcp_vault_secrets_secret" "boundary_service_account" {
   for_each = toset(local.gcve_workspace_identity_tfc_vars)
   app_name    = "boundary-service-account"
   secret_name = each.value
